c7876453dc6394ce9fb5fe8f91b5695171c728ac
athalie-aurora <119656945+athalie-aurora@users.noreply.github.com>
2024-07-19 16:55:31 +0700

    Add Module Form

---
+++ src/components/AddModuleForm.vue
@@ -0,0 +1,101 @@
+<template>
+  <v-dialog v-model="dialog" max-width="450px">
+    <v-card>
+      <v-card-title class="text-h6 d-flex justify-space-between align-center">
+        <span class="pl-5">Tambah Module</span>
+        <v-divider vertical class="pl-16 ml-16"></v-divider>
+
+        <v-btn
+          @click="closeDialog"
+          prepend-icon="mdi-close"
+          variant="plain"
+          class="pr-11"
+        >
+          Close
+        </v-btn>
+      </v-card-title>
+      <v-divider class="my-2"></v-divider>
+      <v-card-text>
+        <v-form ref="form">
+          <v-text-field
+            v-model="newDiv.moduleName"
+            label="Nama Module"
+            placeholder="contoh: authfinnet"
+            variant="outlined"
+            required
+          ></v-text-field>
+          <v-text-field
+            v-model="newDiv.profileModule"
+            label="Nama Profile"
+            placeholder="contoh: 001"
+            variant="outlined"
+          ></v-text-field>
+        </v-form>
+      </v-card-text>
+
+      <v-card-actions class="d-flex justify-center pb-6">
+        <v-btn outlined @click="closeDialog" class="cancel-button px-16">
+          Batalkan
+        </v-btn>
+        <v-btn
+          outlined
+          @click="submitForm"
+          :disabled="!isFormValid"
+          class="save-button px-16"
+        >
+          Simpan
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script setup>
+import { ref, computed } from "vue";
+// eslint-disable-next-line no-unused-vars
+const emit = defineEmits(["addNewDivisi"]);
+const dialog = ref(false);
+const newDiv = ref({
+  divisiName: "",
+});
+
+const openDialog = () => {
+  dialog.value = true;
+};
+
+const closeDialog = () => {
+  dialog.value = false;
+};
+
+const isFormValid = computed(() => {
+  return newDiv.value.divisiName;
+});
+
+const submitForm = () => {
+  if (isFormValid.value) {
+    // eslint-disable-next-line no-undef
+    emit("addNewDiv", { ...newDiv.value });
+
+    // Reset form
+    newDiv.value = {
+      divisiName: "",
+    };
+    closeDialog();
+  }
+};
+
+defineExpose({
+  openDialog,
+});
+</script>
+
+<style scoped>
+.cancel-button {
+  border: 1px solid orange;
+  color: orange;
+}
+.save-button {
+  background-color: #e0e0e0;
+  color: gray;
+}
+</style>
---
+++ src/components/EditModuleForm.vue
@@ -0,0 +1,109 @@
+<template>
+  <v-dialog v-model="dialog" max-width="450px">
+    <v-card>
+      <v-card-title class="text-h6 d-flex justify-space-between align-center">
+        <span class="pl-5">Edit Module</span>
+        <v-divider vertical class="pl-16 ml-16"></v-divider>
+
+        <v-btn
+          @click="closeDialog"
+          prepend-icon="mdi-close"
+          variant="plain"
+          class="pr-11"
+        >
+          Close
+        </v-btn>
+      </v-card-title>
+      <v-divider class="my-2"></v-divider>
+      <v-card-text>
+        <v-form ref="form">
+          <v-text-field
+            v-model="divData.moduleName"
+            label="Nama Module"
+            placeholder="contoh: authfinnet"
+            variant="outlined"
+            required
+          ></v-text-field>
+          <v-text-field
+            v-model="divData.profileModule"
+            label="Nama Profile"
+            placeholder="contoh: 001"
+            variant="outlined"
+            required
+          ></v-text-field>
+        </v-form>
+      </v-card-text>
+
+      <v-card-actions class="d-flex justify-center pb-6">
+        <v-btn outlined @click="closeDialog" class="cancel-button px-16">
+          Batalkan
+        </v-btn>
+        <v-btn
+          outlined
+          @click="submitForm"
+          :disabled="!isFormValid"
+          class="save-button px-16"
+        >
+          Simpan
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script setup>
+import { ref, computed } from "vue";
+
+const emit = defineEmits(["editDiv"]);
+const dialog = ref(false);
+const divData = ref({
+  idDivisi: "",
+  moduleName: "",
+  profileModule: "",
+});
+
+const openDialog = (item) => {
+  divData.value.idDivisi = item.idDivisi;
+  divData.value.moduleName = item.moduleName;
+  divData.value.profileModule = item.profileModule;
+
+  dialog.value = true;
+};
+
+const closeDialog = () => {
+  dialog.value = false;
+};
+
+const isFormValid = computed(() => {
+  return divData.value.moduleName;
+});
+
+const submitForm = () => {
+  if (isFormValid.value) {
+    // eslint-disable-next-line no-undef
+    emit("editDiv", { ...divData.value });
+
+    // Reset form
+    divData.value = {
+      moduleName: "",
+      profileModule: "",
+    };
+    closeDialog();
+  }
+};
+
+defineExpose({
+  openDialog,
+});
+</script>
+
+<style scoped>
+.cancel-button {
+  border: 1px solid orange;
+  color: orange;
+}
+.save-button {
+  background-color: #e0e0e0;
+  color: gray;
+}
+</style>
