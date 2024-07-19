563ce8d525380389a83e6b5506cc4ac89deecc57
athalie-aurora <119656945+athalie-aurora@users.noreply.github.com>
2024-07-19 16:56:03 +0700

    Add Product and Sub Product Form

---
+++ src/components/AddProductForm.vue
@@ -0,0 +1,120 @@
+<template>
+  <v-dialog v-model="dialog" max-width="450px">
+    <v-card>
+      <v-card-title class="text-h6 d-flex justify-space-between align-center">
+        <span class="pl-5">Tambah Produk</span>
+        <v-divider vertical class="pl-16 ml-10"></v-divider>
+
+        <v-btn
+          @click="closeDialog"
+          prepend-icon="mdi-close"
+          variant="plain"
+          class="pr-10"
+        >
+          Close
+        </v-btn>
+      </v-card-title>
+      <v-divider class="my-2"></v-divider>
+      <v-card-text>
+        <v-form ref="form">
+          <v-select
+            label="Pilih Departemen"
+            placeholder="Departemen yang tersedia"
+            variant="outlined"
+          ></v-select>
+          <v-text-field
+            v-model="newDiv.departementName"
+            label="Nama Produk"
+            placeholder="contoh: Mobile Banking"
+            variant="outlined"
+            required
+          ></v-text-field>
+        </v-form>
+      </v-card-text>
+
+      <v-card-actions class="d-flex justify-center pb-6">
+        <v-btn outlined @click="closeDialog" class="cancel-button px-16">
+          Batalkan
+        </v-btn>
+        <v-btn
+          outlined
+          @click="submitForm"
+          :disabled="!isFormValid"
+          class="save-button px-16"
+        >
+          Simpan
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script setup>
+import { ref, computed } from "vue";
+// eslint-disable-next-line no-unused-vars
+const emit = defineEmits(["addNewDepartement"]);
+const dialog = ref(false);
+const newDiv = ref({
+  departementName: "",
+});
+
+const openDialog = () => {
+  dialog.value = true;
+};
+
+const closeDialog = () => {
+  dialog.value = false;
+};
+
+const isFormValid = computed(() => {
+  return newDiv.value.departementName;
+});
+
+const submitForm = () => {
+  if (isFormValid.value) {
+    // eslint-disable-next-line no-undef
+    emit("addNewDiv", { ...newDiv.value });
+
+    // Reset form
+    newDiv.value = {
+      departementName: "",
+    };
+    closeDialog();
+  }
+};
+
+defineExpose({
+  openDialog,
+});
+</script>
+
+<style scoped>
+.cancel-button {
+  border: 1px solid orange;
+  color: orange;
+}
+.save-button {
+  background-color: #e0e0e0;
+  color: gray;
+}
+</style>
+
+<!-- <v-form ref="form">
+          <v-select
+            v-model="newDept.divisionId"
+            :items="divisions"
+            item-text="divisiName"
+            item-value="idDivisi"
+            label="Pilih Divisi"
+            placeholder="Pilih Divisi"
+            variant="outlined"
+            required
+          ></v-select>
+          <v-text-field
+            v-model="newDept.departmentName"
+            label="Nama Departemen"
+            placeholder="contoh: Pengembangan"
+            variant="outlined"
+            required
+          ></v-text-field>
+        </v-form> -->
---
+++ src/components/EditSubProductForm.vue
@@ -0,0 +1,121 @@
+<template>
+  <v-dialog v-model="dialog" max-width="450px">
+    <v-card>
+      <v-card-title class="text-h6 d-flex justify-space-between align-center">
+        <span class="pl-5">Edit Sub Produk</span>
+        <v-divider vertical class="pl-16 ml-16"></v-divider>
+
+        <v-btn
+          @click="closeDialog"
+          prepend-icon="mdi-close"
+          variant="plain"
+          class="pr-11"
+        >
+          Close
+        </v-btn>
+      </v-card-title>
+      <v-divider class="my-2"></v-divider>
+      <v-card-text>
+        <v-form ref="form">
+          <v-select
+            v-model="divData.departementName"
+            label="Nama Departemen"
+            placeholder="contoh: Digital Enterprise"
+            variant="outlined"
+            required
+          ></v-select>
+
+          <v-text-field
+            v-model="divData.productName"
+            label="Nama Produk"
+            placeholder="contoh: Mobile Banking"
+            variant="outlined"
+            required
+          ></v-text-field>
+
+          <v-text-field
+            v-model="divData.subproductName"
+            label="Nama Sub Produk"
+            placeholder="contoh: E-Monitoring"
+            variant="outlined"
+            required
+          ></v-text-field>
+        </v-form>
+      </v-card-text>
+
+      <v-card-actions class="d-flex justify-center pb-6">
+        <v-btn outlined @click="closeDialog" class="cancel-button px-16">
+          Batalkan
+        </v-btn>
+        <v-btn
+          outlined
+          @click="submitForm"
+          :disabled="!isFormValid"
+          class="save-button px-16"
+        >
+          Simpan
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script setup>
+import { ref, computed } from "vue";
+
+const emit = defineEmits(["editDiv"]);
+const dialog = ref(false);
+const divData = ref({
+  idDivisi: "",
+  departementName: "",
+  productName: "",
+  subproductName: "",
+});
+
+const openDialog = (item) => {
+  divData.value.idDivisi = item.idDivisi;
+  divData.value.departementName = item.departementName;
+  divData.value.productName = item.productName;
+  divData.value.subproductName = item.subproductName;
+
+  dialog.value = true;
+};
+
+const closeDialog = () => {
+  dialog.value = false;
+};
+
+const isFormValid = computed(() => {
+  return divData.value.departementName;
+});
+
+const submitForm = () => {
+  if (isFormValid.value) {
+    // eslint-disable-next-line no-undef
+    emit("editDiv", { ...divData.value });
+
+    // Reset form
+    divData.value = {
+      departementName: "",
+      productName: "",
+      subproductName: "",
+    };
+    closeDialog();
+  }
+};
+
+defineExpose({
+  openDialog,
+});
+</script>
+
+<style scoped>
+.cancel-button {
+  border: 1px solid orange;
+  color: orange;
+}
+.save-button {
+  background-color: #e0e0e0;
+  color: gray;
+}
+</style>
